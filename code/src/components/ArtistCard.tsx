a02fbc435ba4c23defb36512383751eb8c7f17da
jhonatan <j.azevedo@ctrlplay.com.br>
2023-10-12 16:09:25 -0300

    home design resposive

--- code/src/components/ArtistCard.tsx
+++ code/src/components/ArtistCard.tsx
@@ -10,14 +10,16 @@ export default function ArtistsCard(props: ArtistsCardProps) {
 
   return (
     <NavLink to={`/guess/${artist.slug}/`}>
-      <div className="relative rounded-md cursor-pointer border-2 border-white/10 h-48 w-full">
+      <div className="relative rounded-md cursor-pointer border-2 border-slate-900/30 h-48 md:h-64 lg:h-80 w-full">
         <img
           className="rounded-md h-full w-full object-cover"
           src={artist.image}
           alt=""
         />
         <div className="absolute p-2 bg-black/60 bottom-0 w-full text-center">
-          <p className="text-white text-xl font-bold">{artist.name}</p>
+          <p className="text-white text-xl md:text-2xl lg:text-3xl font-bold">
+            {artist.name}
+          </p>
         </div>
       </div>
     </NavLink>
--- code/src/pages/Home.tsx
+++ code/src/pages/Home.tsx
@@ -4,9 +4,11 @@ import ARTISTS from "../data/artists";
 
 export default function Home() {
   return (
-    <div className="flex flex-col text-white p-6 gap-4">
-      <h1 className="font-bold text-center text-4xl">Guess the Music</h1>
-      <div className="grid grid-cols-1 gap-6">
+    <div className="flex flex-col text-white p-6 md:p-8 lg:p-12 gap-4 md:gap-6 lg:gap-10">
+      <h1 className="font-bold text-center text-4xl md:text-5xl">
+        Guess the Music
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {ARTISTS.map((artist: ArtistModel) => (
           <ArtistsCard artist={artist} />
         ))}
