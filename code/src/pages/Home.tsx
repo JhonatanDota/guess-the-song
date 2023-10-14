9375192064c6771b0a97a010ad28fab27d60bfff
jhonatan <j.azevedo@ctrlplay.com.br>
2023-10-14 13:39:21 -0300

    some design fixes

--- code/src/pages/Home.tsx
+++ code/src/pages/Home.tsx
@@ -15,11 +15,11 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col text-white p-6 md:p-8 lg:p-12 gap-4 md:gap-6 lg:gap-10">
+    <div className="flex flex-col text-white p-6 md:p-8 lg:p-12 gap-4 md:gap-6">
       <h1 className="font-bold text-center text-4xl md:text-5xl font-display">
         Guess the Music
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {ARTISTS.map((artist: ArtistModel) => (
           <ArtistsCard
             key={artist.slug}
@@ -30,7 +30,7 @@ export default function Home() {
       </div>
 
       <div
-        className={`fixed top-0 left-0 h-screen w-full bg-cover bg-no-repeat z-[-5] transition-opacity delay-75 ${
+        className={`fixed top-0 left-0 h-screen w-full bg-cover bg-no-repeat z-[-5] transition-opacity delay-100 ${
           bgImage ? "opacity-50" : "opacity-0"
         }`}
         style={{ backgroundImage: `url(${bgImage})` }}
