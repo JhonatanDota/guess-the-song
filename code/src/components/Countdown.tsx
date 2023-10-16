8f56b54708a662921fb36f766d6d5b0f5321aac5
jhonatan <j.azevedo@ctrlplay.com.br>
2023-10-16 19:44:43 -0300

    saving countdown design

--- code/src/components/Countdown.tsx
+++ code/src/components/Countdown.tsx
@@ -24,9 +24,9 @@ export default function Countdown(props: CountdownProps) {
   return (
     <>
       {count > 0 && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black z-100">
-          <div className="relative bg-white p-6 md:p-12 lg:p-24 rounded-full">
-            <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl md:text-4xl lg:text-6xl text-black">
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-100">
+          <div className="relative bg-white/80 p-12 md:p-16 lg:p-24 rounded-full">
+            <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl md:text-5xl lg:text-6xl text-black font-bold">
               {count}
             </h1>
           </div>
--- code/src/pages/GuessMusics.tsx
+++ code/src/pages/GuessMusics.tsx
@@ -68,7 +68,7 @@ export default function GuessMusics() {
               onCountdownDone={() => setIsCountdownDone(true)}
             />
           ) : (
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col w-2/4 md:w-1/4 lg:w-1/6 gap-6">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col w-2/4 md:w-1/4 lg:w-1/5 gap-6">
               <button
                 className="uppercase flex justify-between items-center p-2 rounded-md text-2xl md:text-3xl lg:text-4xl text-white bg-[#008000] font-bold"
                 onClick={handleStartRound}
