d3098edd7a30a96b24ed5e7ccc6e4afdcc74fbfe
jhonatan <j.azevedo@ctrlplay.com.br>
2023-11-02 21:05:36 -0300

    add animate pulse on progress

--- code/src/components/MusicProgressBar.tsx
+++ code/src/components/MusicProgressBar.tsx
@@ -3,11 +3,16 @@ type MusicProgressBarProps = {
 };
 
 export default function MusicProgressBar(props: MusicProgressBarProps) {
+  const { progress } = props;
   return (
-    <div className="bg-gray-100/40 rounded-full">
+    <div
+      className={`bg-gray-100/40 rounded-full ${
+        progress === 0 && "animate-pulse"
+      }`}
+    >
       <div
         className="bg-red-400 h-2.5 md:h-3.5 lg:h-5 rounded-full transition-width duration-100 max-w-full"
-        style={{ width: `${props.progress}%` }}
+        style={{ width: `${progress}%` }}
       ></div>
     </div>
   );
